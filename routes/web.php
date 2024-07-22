301f1348cb9ecf6bb9ee5d5d9a4a33acfe8ab4df
Melda211220111 <meldasyahreza42@gmail.com>
2024-07-22 14:38:17 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,26 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\KendaraanController;
-use App\Http\Controllers\KwitansiController;
+
 use App\Http\Controllers\PenyewaController;
+use App\Http\Controllers\KendaraanController;
+use App\Http\Controllers\SewaController;
+use App\Http\Controllers\KwitansiController;
+use App\Http\Controllers\AwalController;
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::resource('kendaraan', KendaraanController::class);
-Route::resource('penyewa', PenyewaController::class);
-Route::resource('kwitansi', KwitansiController::class);
+Route::resource('/login', LoginController::class);
+Route::resource('/penyewa', PenyewaController::class);
+Route::resource('/kendaraan', KendaraanController::class);
+Route::resource('/sewa',SewaController::class);
+Route::resource('/kwitansi',KwitansiController::class);
+Route::resource('/invoice',InvoiceController::class);
+Route::resource('/awal',AwalController::class);
+Route::resource('/user',UserController::class);
+Route::resource('/admin',AdminController::class);
